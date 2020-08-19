4499cbfa8b5b183d883945a91936993c4bd2de5f
gbudau <gbudau@student.42.fr>
2020-08-19 19:08:56 +0200

    Modified ft_strlcpy

--- lib/libft/ft_strlcpy.c
+++ lib/libft/ft_strlcpy.c
@@ -6,32 +6,23 @@
 /*   By: gbudau <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 14:39:35 by gbudau            #+#    #+#             */
-/*   Updated: 2019/12/03 22:10:41 by gbudau           ###   ########.fr       */
+/*   Updated: 2020/08/10 19:20:56 by gbudau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize)
+size_t		ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	size_t i;
-	size_t j;
+	const char *start;
 
-	if (dstsize == 0)
-		return (ft_strlen(src));
-	if (dst == NULL)
-		return (0);
-	i = 0;
-	j = 0;
-	while (src[i])
-	{
-		if (j < dstsize - 1)
-		{
-			dst[j] = src[j];
-			j++;
-		}
-		i++;
-	}
-	dst[j] = '\0';
-	return (i);
+	if (size == 0)
+		return ft_strlen(src);
+	start = src;
+	while (*src && size-- > 1)
+		*dst++ = *src++;
+	*dst = '\0';
+	while (*src)
+		src++;
+	return src - start;
 }
